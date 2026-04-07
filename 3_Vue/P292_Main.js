1f3cd2737fc29cd0c2b1817e35abcdfaf6112c36
Beom Sung Heo <48181483+hbsowo58@users.noreply.github.com>
2026-04-07 09:20:31 +0900

    fix : fix typo

--- 3_Vue/P292_Main.js
+++ 3_Vue/P292_Main.js
@@ -2,6 +2,7 @@ import "./assets/main.css";
 
 import { createApp } from "vue";
 import App from "./App.vue";
+import 'echarts';
 
 // import * as echarts from "echarts";
 import VueECharts from "vue-echarts";
